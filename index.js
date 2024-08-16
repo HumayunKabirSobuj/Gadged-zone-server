07d9bc25a98bdb99005e2778884041bf637534b3
humayunkabir506034 <humayunkabir506034@gmail.com>
2024-08-16 18:58:45 +0600

    pagination functionality added

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 var cors = require('cors')
@@ -21,6 +20,7 @@ app.use(express.json())
 // console.log(process.env.DB_USER)
 // console.log(process.env.DB_PASS)
 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9b6e9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9b6e9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,16 +37,74 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
-        
 
-       
+        const database = client.db("GadgetZone");
+        const phonesCollection = database.collection("phones");
+
+
+        app.get('/phones', async (req, res) => {
+            const brand = req.query.brand; // Get brand from query params
+            const category = req.query.category; // Get category from query params
+            const name = req.query.productname; // Get category from query params
+            console.log({ brand, category, name });
+            // const productname=name.toLocaleLowerCase()
+
+
+
+            let query = {};
+
+            if (brand && brand !== 'null') {
+                query.brand = brand;
+            }
+
+            if (category && category !== 'null') {
+                query.category = category;
+            }
+
+            if (name) {
+                query.productname = { $regex: name, $options: "i" };
+            }
+
+
+
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const allProducts = await phonesCollection.find()
+
+                .skip(page * size)
+                .limit(size)
+                .toArray();
+
+
+
+
+
+            try {
+
+                // const result = await phonesCollection.find(query).toArray();
+                // res.send(result);
+                const count = await phonesCollection.countDocuments()
+                // console.log(allProducts, count)
+
+                res.send({
+                    phones: allProducts,
+                    count: count
+                })
+            } catch (error) {
+                console.error(error);
+                res.status(500).send({ message: "Internal Server Error" });
+            }
+        });
+
+
+
 
 
 
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
+        await client.db("admin").command({ ping: 1 });
 
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -55,7 +113,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Assignment 12 server running')
+    res.send('Gadged Zone server is running')
 })
 
 app.listen(port, () => {
