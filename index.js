066aaaaf16056e4958b0e130ce1f6616b2be822c
humayunkabir506034 <humayunkabir506034@gmail.com>
2024-08-17 16:38:44 +0600

    complete

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "http://localhost:5174",
+            "https://gadgedzonescic10.netlify.app"
             
         ],
         credentials: true,
